7b8d00f507b7624bebd8e2db36ebbce868c18d6d
mail2anagesh <mail2anagesh@gmail.com>
2024-12-12 08:26:38 +0000

    clean up

--- src/main/java/com/jpmorgan/gbce/stockmarket/config/Secret.java
+++ src/main/java/com/jpmorgan/gbce/stockmarket/config/Secret.java
@@ -2,6 +2,6 @@ package com.jpmorgan.gbce.stockmarket.config;
 
 public class Secret {
 
-    public String userName = "demo1";
-    public String passWord = "pass1234";
+    public String userName;
+    public String passWord;
 }
